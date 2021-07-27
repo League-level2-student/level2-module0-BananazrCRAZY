293e99b65a97d860b08943a45970166c4af6008c
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2021-07-26 19:59:58 -0700

    arrays

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,19 +3,24 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String strings[] = {"Hi", "I", "am", "super", "COOl"};
 		//2. print the third element in the array
-		
+		System.out.println(strings[3]);
 		//3. set the third element to a different value
-		
+		strings[3] = "SUPPPPER";
 		//4. print the third element again
-		
+		System.out.println(strings[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int integers[];
+		for (int i = 0; i < 50; i++) {
+			//integers[i] = i;
+		}
 		//7. use a for loop to make every value of the integer array a random number
 		
 		//8. without printing the entire array, print only the smallest number in the array
